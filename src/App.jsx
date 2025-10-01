7a70318a75a449142fc93e5f9c326260572fe3c4
nuwquynn8305 <dinhq9392@gmail.com>
2025-10-01 13:34:57 +0700

    file khởi tạo dự án React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Xin chào React + Vite + Tailwind 🚀
+      </h1>
+    </div>
+  );
+}
